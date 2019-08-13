3e1b6b8ea34ba84d4c6081ce24ff58b953244875
Jorge Luis Gutierrez Fernandez <jorge.gutierrezf3@gmail.com>
2019-08-13 10:25:02 -0400

    pruebas de integracion

---
+++ BTPrinter.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using BTPrinter.BE.Negocio.Reglas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BTPrinter.Test
+{
+    [TestClass]
+    public class GestionDispositivosTest
+    {
+        [TestMethod]
+        public void IniciarRecepcionImagenBT()
+        {
+            GestionDispositivos gestion = new GestionDispositivos();
+            MemoryStream archivo = gestion.IniciarRecepcionImagenBT();
+            Assert.IsNotNull(archivo);
+        }
+    }
+}
